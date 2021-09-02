e9e5c581279012007b8d9fc61e91a5f16884c3b9
adiamante <dmnt123@gmail.com>
2021-09-01 19:21:25 -0600

    add YeetOverFlow.Wpf.Demo project

---
+++ YeetOverFlow.Wpf.Demo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using YeetOverFlow.Wpf.ServiceCollectionExtensions;
+
+namespace YeetOverFlow.Wpf.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            //https://executecommands.com/dependency-injection-in-wpf-net-core-csharp/
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+            _serviceProvider.InitYeetWpf();         //Initialize wpf app
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<DemoWindow>();
+            services.AddYeetWpf();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var demoWindow = _serviceProvider.GetService<DemoWindow>();
+            demoWindow.Show();
+        }
+    }
+}
---
+++ YeetOverFlow.Wpf.Demo/DemoWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using YeetOverFlow.Wpf.Controls;
+using YeetOverFlow.Wpf.ViewModels;
+
+namespace YeetOverFlow.Wpf.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DemoWindow : YeetWindow
+    {
+        public DemoWindow(YeetWindowViewModel vm) : base(vm)
+        {
+            InitializeComponent();
+        }
+    }
+}
