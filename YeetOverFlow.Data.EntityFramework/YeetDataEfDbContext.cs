da9b629ea0eb24fa4b64d0aa50cb315688b993ab
Angelo Diamante <angelo.diamante@tamucc.edu>
2021-09-08 14:10:12 -0500

    add YeetOverFlow.Data.EntityFramework project

---
+++ YeetOverFlow.Data.EntityFramework/YeetDataEfDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YeetOverFlow.Core.EntityFramework;
+
+namespace YeetOverFlow.Data.EntityFramework
+{
+    public class YeetDataEfDbContext : YeetEfDbContext<YeetDataSet, YeetData>
+    {
+        public YeetDataEfDbContext(DbContextOptions<YeetDataEfDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new YeetDataEntityConfiguration());
+        }
+    }
+
+    //internal class YeetDataSetEntityConfiguration : IEntityTypeConfiguration<YeetDataSet>
+    //{
+    //    public void Configure(EntityTypeBuilder<YeetDataSet> builder)
+    //    {
+    //        builder.ToTable(nameof(YeetDataSet));
+    //    }
+    //}
+
+    internal class YeetDataEntityConfiguration : IEntityTypeConfiguration<YeetData>
+    {
+        public void Configure(EntityTypeBuilder<YeetData> builder)
+        {
+            builder.ToTable(nameof(YeetData));
+            builder.Property(itm => itm.Key).HasField("_key");
+        }
+    }
+
+}
--- YeetOverFlow.Data/YeetDataSet.cs
+++ YeetOverFlow.Data/YeetDataSet.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using YeetOverFlow.Core;
 
 namespace YeetOverFlow.Data
 {
-    public class YeetDataSet : YeetData
+    public class YeetDataSet : YeetData, IYeetKeyedList<YeetData>
     {
+        protected YeetKeyedList<YeetDataSet, YeetData> _yeetKeyedList;
+
         public YeetDataSet() : base()
         {
 
@@ -12,6 +16,54 @@ namespace YeetOverFlow.Data
         public YeetDataSet(Guid guid, string key) : base(guid, key)
         {
             _key = key;
+        }
+
+        public YeetData this[string key] { get => ((IYeetKeyedList<YeetData>)_yeetKeyedList)[key]; set => ((IYeetKeyedList<YeetData>)_yeetKeyedList)[key] = value; }
+
+        public YeetData this[int key] => ((IYeetListBase<YeetData>)_yeetKeyedList)[key];
+
+        public IEnumerable<YeetData> Children => ((IYeetListBaseRead<YeetData>)_yeetKeyedList).Children;
+
+        public int Count => ((IYeetListBaseRead<YeetData>)_yeetKeyedList).Count;
+
+        public void AddChild(YeetData newChild)
+        {
+            ((IYeetListBaseWrite<YeetData>)_yeetKeyedList).AddChild(newChild);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return ((IYeetKeyedList<YeetData>)_yeetKeyedList).ContainsKey(key);
+        }
+
+        public void Init()
+        {
+            ((IYeetKeyedList<YeetData>)_yeetKeyedList).Init();
+        }
+
+        public void InsertChildAt(int targetSequence, YeetData newChild)
+        {
+            ((IYeetListBaseWrite<YeetData>)_yeetKeyedList).InsertChildAt(targetSequence, newChild);
+        }
+
+        public void MoveChild(int targetSequence, YeetData childToMove)
+        {
+            ((IYeetListBaseWrite<YeetData>)_yeetKeyedList).MoveChild(targetSequence, childToMove);
+        }
+
+        public void Remove(string keyToRemove)
+        {
+            ((IYeetKeyedList<YeetData>)_yeetKeyedList).Remove(keyToRemove);
+        }
+
+        public void RemoveChild(YeetData childToRemove)
+        {
+            ((IYeetListBaseWrite<YeetData>)_yeetKeyedList).RemoveChild(childToRemove);
+        }
+
+        public void RemoveChildAt(int targetSequence)
+        {
+            ((IYeetListBaseWrite<YeetData>)_yeetKeyedList).RemoveChildAt(targetSequence);
         }
     }
 }
