9232ceb378220f49eb39b44d7722961a8c1e70d0
Angelo Diamante <angelo.diamante@tamucc.edu>
2021-09-13 12:14:29 -0500

    add YeetOverFlow.Data.Wpf project

---
+++ YeetOverFlow.Data.Wpf/YeetDataWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using YeetOverFlow.Wpf.Controls;
+using YeetOverFlow.Wpf.ViewModels;
+
+namespace YeetOverFlow.Data.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class YeetDataWindow : YeetWindow
+    {
+        public YeetDataWindow(YeetWindowViewModel vm) : base(vm)
+        {
+            InitializeComponent();
+        }
+    }
+}
