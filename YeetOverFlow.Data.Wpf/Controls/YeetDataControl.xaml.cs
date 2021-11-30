1d52fe551b033a46e1b833bac8d581dc072689d0
adiamante <angelo.c.diamante@gmail.com>
2021-11-29 19:08:51 -0600

    [YeetOverFlow.Data.Wpf]  add tsv paste parsing

--- YeetOverFlow.Data.Wpf/Controls/YeetDataControl.xaml.cs
+++ YeetOverFlow.Data.Wpf/Controls/YeetDataControl.xaml.cs
@@ -5,6 +5,8 @@ using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using YeetOverFlow.Data.Wpf.ViewModels;
 using YeetOverFlow.Wpf.Controls;
 
@@ -31,108 +33,160 @@ namespace YeetOverFlow.Data.Wpf.Controls
             InitializeComponent();
         }
 
-        private void Grid_Drop(object sender, DragEventArgs e)
+        private void MainPanel_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 var files = ((string[])e.Data.GetData(DataFormats.FileDrop));
-                foreach (var file in files)
+                foreach (var filePath in files)
                 {
-                    String filename = Path.GetFileName(file);
-                    String ext = Path.GetExtension(file).TrimStart('.');
-
-                    //YeetTableViewModel tbl = new YeetTableViewModel();
-                    YeetTableViewModel tbl = new YeetTableViewModel(Guid.NewGuid(), filename);
-                    tbl.Init();
-                    Data.Root.AddChild(tbl);
+                    String filename = Path.GetFileName(filePath);
+                    String ext = Path.GetExtension(filePath).TrimStart('.');
 
                     switch (ext.ToLower())
                     {
                         case "csv":
-                            var fieldDelim = ',';
-                            var recordDelim = '\n';
-                            var hasHeaders = true;
-                            CsvConfiguration _conf = new CsvConfiguration(CultureInfo.InvariantCulture);
-                            var fs = File.OpenRead(file);
-                            var sr = new StreamReader(fs);
-                            fs.Seek(0, SeekOrigin.Begin); // <-- missing line
-
-                            if (recordDelim != '\n')
-                            {
-                                #region If Record Delimiter is overriden, itterate through all characters and replace them with new line
-                                //https://stackoverflow.com/questions/1232443/writing-to-then-reading-from-a-memorystream
-                                MemoryStream ms = new MemoryStream();
-
-                                StreamWriter sw = new StreamWriter(ms);
-                                while (sr.Peek() >= 0)
-                                {
-                                    Char c = (Char)sr.Read();
-                                    if (c == recordDelim)
-                                    {
-                                        sw.Write('\n');
-                                    }
-                                    else
-                                    {
-                                        sw.Write(c);
-                                    }
-                                }
-
-                                sw.Flush();
-                                ms.Position = 0;
-
-                                sr = new StreamReader(ms, Encoding.UTF8);
-                                #endregion
-                            }
-
-                            _conf.BadDataFound = null;
-                            //_conf.BadDataFound = cxt =>
-                            //{
-                            //    //For debugging(put breakpoints here)
-                            //};
-
-                            _conf.Delimiter = fieldDelim.ToString();
-                            if (_conf.Delimiter != ",")
-                            {
-                                _conf.Mode = CsvHelper.CsvMode.NoEscape;
-                            }
-                            _conf.HasHeaderRecord = hasHeaders;
-                            _conf.MissingFieldFound = null;
-                            CsvReader csvReader = new CsvReader(sr, _conf);
-                            CsvDataReader dataReader = new CsvDataReader(csvReader);
-
-                            if (hasHeaders)
-                            {
-                                csvReader.ReadHeader();
-                            }
-
-                            foreach (var header in csvReader.HeaderRecord)
-                            {
-                                var col = new YeetStringColumnViewModel();
-                                tbl.Columns[header.Trim().Trim('"')] = col;
-                            }
-
-                            while (csvReader.Read())
-                            {
-                                var row = new YeetRowViewModel();
-                                foreach (var header in csvReader.HeaderRecord)
-                                {
-                                    if (csvReader.TryGetField<String>(header, out string field))
-                                    {
-                                        var cell = new YeetStringCellViewModel();
-                                        cell.Value = field;
-                                        row[header.Trim().Trim('"')] = cell;
-                                    }
-                                }
-                                tbl.Rows.AddChild(row);
-                            }
+                            Data.Root[filename] = YeetDataConverter.CsvFileToData(filePath);
+                            break;
+                        case "tsv":
+                            Data.Root[filename] = YeetDataConverter.CsvFileToData(filePath, new YeetDataConverterOptions() { FieldDelim = '\t' });
                             break;
                     }
+                }
+            }
+        }
 
-                    //Data.Root[filename] = tbl;
-                    //Data.Root.AddChild(tbl);
+        private void PasteTextBox_PreviewExecuted(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Paste)
+            {
+                var tabItem = (TabItem)((FrameworkElement)sender).Parent;
+                String text = Clipboard.GetText();
+                String name = $"Tbl {Data.Root.Count + 1}";
+
+                switch (tabItem.Header.ToString().ToLower())
+                {
+                    case "csv":
+                        Data.Root[name] = YeetDataConverter.CsvStringToData(text);
+                        break;
+                    case "tsv":
+                        Data.Root[name] = YeetDataConverter.CsvStringToData(text, new YeetDataConverterOptions() { FieldDelim = '\t' });
+                        break;
+                }
+                
+                e.Handled = true;
+            }
+        }
+    }
+
+    internal class YeetDataConverterOptions
+    {
+        public string Name { get; set; }
+        public char RecordDelim { get; set; } = '\n';
+        public char FieldDelim { get; set; } = ',';
+        public bool HasHeaders { get; set; } = true;
+    }
+
+    internal static class YeetDataConverter
+    {
+        public static YeetDataViewModel CsvStringToData(string csv, YeetDataConverterOptions opts = null)
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(csv));
+            return YeetDataConverter.CsvStreamToData(stream, opts);
+
+        }
+        public static YeetDataViewModel CsvFileToData(string filePath, YeetDataConverterOptions opts = null)
+        {
+            var fileStream = File.OpenRead(filePath);
+            fileStream.Seek(0, SeekOrigin.Begin);
+            return YeetDataConverter.CsvStreamToData(fileStream, opts);
+        }
+
+        public static YeetDataViewModel CsvStreamToData(Stream stream, YeetDataConverterOptions opts = null)
+        {
+            if (opts == null)
+            {
+                opts = new YeetDataConverterOptions();
+            }
+
+            var fieldDelim = opts.FieldDelim;
+            var recordDelim = opts.RecordDelim;
+            var hasHeaders = opts.HasHeaders;
+            CsvConfiguration _conf = new CsvConfiguration(CultureInfo.InvariantCulture);
+
+            YeetTableViewModel tbl = new YeetTableViewModel();
+            var sr = new StreamReader(stream);
+
+            if (recordDelim != '\n')
+            {
+                #region If Record Delimiter is overriden, itterate through all characters and replace them with new line
+                //https://stackoverflow.com/questions/1232443/writing-to-then-reading-from-a-memorystream
+                MemoryStream ms = new MemoryStream();
+
+                StreamWriter sw = new StreamWriter(ms);
+                while (sr.Peek() >= 0)
+                {
+                    Char c = (Char)sr.Read();
+                    if (c == recordDelim)
+                    {
+                        sw.Write('\n');
+                    }
+                    else
+                    {
+                        sw.Write(c);
+                    }
                 }
 
+                sw.Flush();
+                ms.Position = 0;
+
+                sr = new StreamReader(ms, Encoding.UTF8);
+                #endregion
             }
+
+            _conf.BadDataFound = null;
+            //_conf.BadDataFound = cxt =>
+            //{
+            //    //For debugging(put breakpoints here)
+            //};
+
+            _conf.Delimiter = fieldDelim.ToString();
+            if (_conf.Delimiter != ",")
+            {
+                _conf.Mode = CsvHelper.CsvMode.NoEscape;
+            }
+            _conf.HasHeaderRecord = hasHeaders;
+            _conf.MissingFieldFound = null;
+            CsvReader csvReader = new CsvReader(sr, _conf);
+            CsvDataReader dataReader = new CsvDataReader(csvReader);
+
+            if (hasHeaders)
+            {
+                csvReader.ReadHeader();
+            }
+
+            foreach (var header in csvReader.HeaderRecord)
+            {
+                var col = new YeetStringColumnViewModel();
+                tbl.Columns[header.Trim().Trim('"')] = col;
+            }
+
+            while (csvReader.Read())
+            {
+                var row = new YeetRowViewModel();
+                foreach (var header in csvReader.HeaderRecord)
+                {
+                    if (csvReader.TryGetField<String>(header, out string field))
+                    {
+                        var cell = new YeetStringCellViewModel();
+                        cell.Value = field;
+                        row[header.Trim().Trim('"')] = cell;
+                    }
+                }
+                tbl.Rows.AddChild(row);
+            }
+
+            return tbl;
         }
     }
 }
